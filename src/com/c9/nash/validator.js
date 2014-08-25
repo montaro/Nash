c1f720b55f36298375f310bad27632ceaf835a14
arefaey <ahmed.rifaey@gmail.com>
2014-08-25 10:10:38 +0300

    JS writes a file on File System as a side effect

--- src/com/c9/nash/validator.js
+++ src/com/c9/nash/validator.js
@@ -1,3 +1,10 @@
+function kill() {
+    var file = new java.io.FileWriter("tess.txt");
+    file.append("Mohammed");
+    file.flush();
+}
+
 function validate(value) {
+ kill()
  return parseInt(value) > 10
  }
